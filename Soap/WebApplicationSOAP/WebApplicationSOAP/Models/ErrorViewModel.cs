0d0d64b743d2bc19e528c43c2857e4cad21c2f33
Cristobal Gonzalez <cristobalgl46@gmail.com>
2021-04-29 19:31:28 -0400

    Inicializacion Cliente

---
+++ Soap/WebApplicationSOAP/WebApplicationSOAP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationSOAP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
