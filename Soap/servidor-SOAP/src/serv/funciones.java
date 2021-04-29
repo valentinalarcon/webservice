8f4bd0073cd0355b2ea45b19870d6907de5338f2
Valentina Alarcón <63269085+valentinalarcon@users.noreply.github.com>
2021-04-29 03:24:27 -0400

    servidor v2

--- Soap/servidor-SOAP/src/serv/funciones.java
+++ Soap/servidor-SOAP/src/serv/funciones.java
@@ -1,8 +1,11 @@
 package serv;
 
+
 public class funciones {
 	public static void main(String args[]) {
         System.out.println(dv_rut("19844152"));
+        System.out.println(separar_nombres("Valentina Ignacia Tobar Alarcon"));
+        System.out.println(separar_apellidos("Valentina Ignacia Tobar Alarcon"));
         
     }
 	public static String dv_rut (String Rut) {
@@ -60,5 +63,66 @@ public class funciones {
 		}
 		return Rut + "-" + dv;
 	}
+	public static String validar_nombre(String Nombre) {
+		if(Nombre.length() == 0) {
+			return "Ingrese un nombre";
+		}
+		else{
+			int cont = 0;
+			for(int i=0; i<Nombre.length(); i++) {
+				char character = Nombre.charAt(i);
+				int ascii = (int) character;
+				if(ascii == 32) {
+					cont++;
+				}
+				for(int j=65; j<=90; j++) {
+					if(ascii == j) {
+						cont++;
+					}
+				}
+				for(int h=97; h<=122; h++) {
+					if(ascii == h) {
+						cont++;
+					}
+				}
+			}
+			if(cont == Nombre.length()) {
+				return "Nombre valido";
+			}
+			else {
+				return "Nombre invalido";
+			}
+				
+		}
+	}
+	
+	public static String separar_nombres(String Nombre) {
+		String[] nombre_separado = Nombre.split(" ");
+		String nombres = "";
+		if(validar_nombre(Nombre)=="Nombre valido") {
+			for(int i=0; i<=nombre_separado.length-3; i++){
+				nombres+=nombre_separado[i] + " ";
+			}
+			return nombres;
+		}
+		else {
+
+			return validar_nombre(Nombre);
+		}
+	}
+	public static String separar_apellidos(String Nombre) {
+		String[] nombre_separado = Nombre.split(" ");
+		String apellidos = "";
+		if(validar_nombre(Nombre)=="Nombre valido") {
+			for(int i=2; i<nombre_separado.length; i++){
+				apellidos+=nombre_separado[i] + " ";
+			}
+			return apellidos;
+		}
+		else {
+			return validar_nombre(Nombre);
+		}
+	}
 }
+
 
