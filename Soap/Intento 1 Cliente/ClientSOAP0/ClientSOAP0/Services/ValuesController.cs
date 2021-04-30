43bee79747bf516411367877b80463dc5ca0f17a
francisco-dh <francisco.diazh@utem.cl>
2021-04-30 02:04:34 -0400

    Intento 1 Cliente SOAP
    veamos si resulta pa mañana

---
+++ Soap/Intento 1 Cliente/ClientSOAP0/ClientSOAP0/Services/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using ClientSOAP0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClientSOAP0.Services
+{
+    public class ServicioSoap
+    {
+        public RutSOAP DVrut(string rut)
+        {
+            ServiceReference1.funcionesClient run = new ServiceReference1.funcionesClient();
+            if(rut == null)
+            {
+                rut = "";
+            }
+            return new RutSOAP()
+            {
+                Rut = run.dv_rut(DVrut)
+            };
+        }
+        public 
+    }
+}
